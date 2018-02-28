cb6d80c2b97751c00864d29a959521f5dc1247cc
Roman Bruckner <bruckner.roman@gmail.com>
2018-02-28 19:02:20 +0100

    update

--- index.js
+++ index.js
@@ -10,10 +10,17 @@ const PADDING = 20;
 
   await page.goto('http://localhost:8080/index.html', { waitUntil: 'networkidle2' });
 
+  // Alternative, which requires no localhost
+  // const MAX_SIZE_PX = 15000;
+  // const FILE_NAME = 'diagram.svg';
+  // const fs = require('fs');
+  // const imageDataURI = 'data:image/svg+xml;base64,' + new Buffer(fs.readFileSync(FILE_NAME)).toString('base64');;
+  // const html = `<img id="${IMAGE_ID}" src="${imageDataURI}" style="max-width: ${MAX_SIZE_PX}px; max-height: ${MAX_SIZE_PX}px"></img>`;
+  // await page.setContent(html);
+
   const dimensions = await page.evaluate(id => {
     const element = document.getElementById(id);
-    const { width, height } = element.getBoundingClientRect();
-    return { width, height };
+    return { width: element.clientWidth, height: element.clientHeight };
   }, IMAGE_ID);
 
   console.log('Dimensions:', dimensions);
